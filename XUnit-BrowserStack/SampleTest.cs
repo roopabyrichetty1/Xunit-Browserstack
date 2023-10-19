5ce712d6102b2f230f3059a2b56b23ba8bda35c9
Roopa Byrichetty <roopab77@yahoo.com>
2023-10-19 11:42:26 -0400

    Updated TEstcase

--- XUnit-BrowserStack/SampleTest.cs
+++ XUnit-BrowserStack/SampleTest.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
 
 namespace XUnit_BrowserStack
 {
@@ -19,13 +20,19 @@ namespace XUnit_BrowserStack
         [Trait("Category", "sample-test")]
         public void BStackSampleTest()
         {
-            RemoteWebDriver driver = baseFixture.GetDriver("chrome", "single");
+            //RemoteWebDriver driver = baseFixture.GetDriver("chrome", "single");
             //WebDriverWait webDriverWait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(2000));
             //driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.bentley.com/");
+            //driver.Navigate().GoToUrl("https://www.bentley.com/");
             //Assert.Equal("Home | Bentley | Infrastructure Engineering Software", driver.Title);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            //chromeOptions.setCapabili("browserVersion", "74");
+            //chromeOptions.setCapability("platformName", "Windows 10");
+            WebDriver driver = new RemoteWebDriver(new Uri("http://www.myexamplebrowserstack.com"), chromeOptions);
+            driver.Navigate().GoToUrl("https://www.bentley.com/");
 
-           
+
+
         }
     }
 }
